e14513e200f31f581d3851e2561fe67fd65098f6
Martonvok <martonsom@gmail.com>
2023-04-10 13:28:57 +0200

    Tétel: maximum kiválasztás

--- src/progitetelek/Progitetelek.java
+++ src/progitetelek/Progitetelek.java
@@ -6,7 +6,8 @@ public class Progitetelek {
 
     
     public static void main(String[] args) {
-       
     }
+
+    
     
 }
--- src/progitetelek/tetelek.java
+++ src/progitetelek/tetelek.java
@@ -1,13 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package progitetelek;
 
-/**
- *
- * @author Marci
- */
 public class tetelek {
-    
+
+    public static void main(String[] args) {
+        int szamok[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        maximum(szamok);
+    }
+
+    public static void maximum(int szamok[]) {
+        int max = 0;
+        for (int i = 0; i < szamok.length; i++) {
+            if (szamok[i] > max) {
+                max = szamok[i];
+            }
+        }
+        System.out.printf("Legnagyobb szám a tömbben: " + max+" ");
+    }
 }

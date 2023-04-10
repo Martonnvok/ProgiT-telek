4ae215db2889bd7f08ff229a8b4a1420191fabeb
Martonvok <martonsom@gmail.com>
2023-04-10 13:36:21 +0200

    Tétel: összegzés

--- src/progitetelek/tetelek.java
+++ src/progitetelek/tetelek.java
@@ -3,9 +3,16 @@ package progitetelek;
 public class tetelek {
 
     public static void main(String[] args) {
-        int szamok[] = {-1, 2, 3, 4, 5, 6, 7, 8, -9, 0};
-        maximum(szamok);
-        minimum(szamok);
+        //tömbök
+        int maxmin[] = {-1, 2, 3, 4, 5, 6, 7, 8, -9, 0};
+        int osszead[]={1,2,3};
+        
+        //kiválaszt
+        maximum(maxmin);
+        minimum(maxmin);
+        
+        //osszead,megszámól
+        osszegzes(osszead);
     }
 
     public static void maximum(int szamok[]) {
@@ -15,10 +22,10 @@ public class tetelek {
                 max = szamok[i];
             }
         }
-        System.out.printf("Legnagyobb szám a tömbben: " + max+" ");
+        System.out.printf("Legnagyobb szám a tömbben: " + max + ". ");
         System.out.println(" ");
     }
-    
+
     public static void minimum(int szamok[]) {
         int min = 0;
         for (int i = 0; i < szamok.length; i++) {
@@ -26,6 +33,28 @@ public class tetelek {
                 min = szamok[i];
             }
         }
-        System.out.printf("Legkisebb szám a tömbben: " + min+" ");
+        System.out.printf("Legkisebb szám a tömbben: " + min + ". ");
+        System.out.println(" ");
     }
+
+    public static void osszegzes(int szamok[]) {
+        int osszeg = 0;
+        for (int i = 0; i < szamok.length; i++) {
+            osszeg = osszeg + szamok[i];
+        }
+        System.out.printf("A számok összege: " + osszeg + ". ");
+        System.out.println(" ");
+    }
+
+    public static void megszamlalas(int szamok[]) {
+        int min = 0;
+        for (int i = 0; i < szamok.length; i++) {
+            if (szamok[i] < min) {
+                min = szamok[i];
+            }
+        }
+        System.out.printf("Legkisebb szám a tömbben: " + min + " ");
+        System.out.println(" ");
+    }
+
 }

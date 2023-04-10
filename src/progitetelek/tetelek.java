c2dbee32eabf94185bec9a6b9f90f3092de5276b
Martonvok <martonsom@gmail.com>
2023-04-10 13:31:04 +0200

    Tétel: minimum kiválasztás

--- src/progitetelek/tetelek.java
+++ src/progitetelek/tetelek.java
@@ -3,8 +3,9 @@ package progitetelek;
 public class tetelek {
 
     public static void main(String[] args) {
-        int szamok[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        int szamok[] = {-1, 2, 3, 4, 5, 6, 7, 8, -9, 0};
         maximum(szamok);
+        minimum(szamok);
     }
 
     public static void maximum(int szamok[]) {
@@ -15,5 +16,16 @@ public class tetelek {
             }
         }
         System.out.printf("Legnagyobb szám a tömbben: " + max+" ");
+        System.out.println(" ");
+    }
+    
+    public static void minimum(int szamok[]) {
+        int min = 0;
+        for (int i = 0; i < szamok.length; i++) {
+            if (szamok[i] < min) {
+                min = szamok[i];
+            }
+        }
+        System.out.printf("Legkisebb szám a tömbben: " + min+" ");
     }
 }
